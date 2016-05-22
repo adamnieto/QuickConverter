f3cd8559f8dce00ace9b11ec803eb47b4a8aa4e3
anieto1 <anieto1@binghamton.edu>
2016-05-22 13:56:14 -0400

    inputValidation.py is near at its final stage

--- quick_lc3_converter/inputValidation.py
+++ quick_lc3_converter/inputValidation.py
@@ -17,43 +17,8 @@ def hexCheck(hexString):
         return case1
     else:
         return False
-def lc3Check(lc3String):
-    #'^' and '$' match the string exactly
 
-    #put the patterns in a list and then iterate over them in re.compile and
-
-    #TODO Make sure to strip any spaces while matching. that is how regex has it.
-    #TODO make sure to have something for hex values. So while it checks for '#-16 - #15' also have those acceptable values in hex after the patterns four loop.
-    patterns =
-    {
-        "^[ADD][R][0-7][,][R][0-7][,][#]-[1-16]$":'add1',
-        "^[ADD][R][0-7][,][R][0-7][,][#][0-15]$": "add2",
-        "^[ADD][R][0-7][,][R][0-7][,][R][0-7]$": "add3",
-        "^[AND][R][0-7][,][R][0-7][,][#]-[1-16]$":'and1',
-        "^[AND][R][0-7][,][R][0-7][,][#][0-15]$": "and2",
-        "^"
-
-
-
-
-
-
-    }
-
-
-    re.compile(patterns[i])
-
-    # oppCodes = ["ADD", "AND", "BR", "JMP", "JSR", "JSRR", "LD", "LDI", "LDR",
-    #             "LEA", "NOT", "RET", "RTI", "ST", "STI", "STR", "TRAP"]
-    # splitString = lc3String.split(" ")
-    # if splitString[0] in oppCodes and splitString[0] is "NOT":
-    #     lc3String = splitString[1:]
-    #     case1 = bool(re.search(pattern1, lc3String))
-    #     #TODO need to add pattern for lc3 code
-
-
-
-def driver(hexString, binString, lc3String):
+def driver(hexString, binString):
     if hexString != "":
         if hexCheck(hexString):
             return hexString
@@ -67,12 +32,7 @@ def driver(hexString, binString, lc3String):
         else:
             return "-1"
     else:
-        if lc3Check(lc3String):
-            return lc3String
-        else:
-            return "-1"
-# TODO: Need to add lc3 validation checker.
+        # This means no input was given just empty strings
+        return "-2"
 
-
-    # checkLC3 = LC3Check(lc3String)
-driver("0xffff","0001 0000 0001 1111", "ADD R1, R1, #0")
+driver("0xffff","0001 0000 0001 1111")
