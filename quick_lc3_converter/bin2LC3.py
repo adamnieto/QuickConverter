ab4678cb24303fc9090420cbadceef4ebafceb2f
anieto1 <anieto1@binghamton.edu>
2016-05-23 12:55:28 -0400

    hex2LC3.py in it final stage

--- quick_lc3_converter/bin2LC3.py
+++
@@ -1,7 +0,0 @@
-
-def hex2Bin(hexString):
-    binNumber = "{0:16b}".format(int(hexString,16))
-    # strip any unecessary space from format add 0
-    # to front and cut at 16th bit
-    return "0" + binNumber.strip(" ")[:17]
-print(hex2Bin("5020"))
--- quick_lc3_converter/hex2LC3.py
+++ quick_lc3_converter/hex2LC3.py
@@ -1,91 +1,20 @@
+import re
 """
 Author: Adam Nieto
-(c) 2016
 
+TODO: Might want to get rid of the get function for dictionaries because will
+      return -1 if false.
+TODO: Make sure that this error goes through the gui
 """
-import re, binascii
-# def fltr2Prcs1(binNumber):
-#
-# def flt1Prcs1(lc3Exprsn, binNumber):
-#     fltrPrcs2Access = ["ADD","AND","NOT", ""]
-#     registerSeq = binNumber[4:7]
-#     registers = {
-#               "000":"R0", "001":"R1", "010":"R2", "011":"R3", "100":"R4",
-#               "101":"R5", "110":"R6", "111":"R7"
-#               }
-#     resultExprsn = lc3Exprsn + registers.get(registerSeq, "-1")
-#     if resultExprsn is "-1":
-#         return False
-#     else:
-#         fltr1Prcs2(lc3Exprsn, binNumber)
-#
-# def filters(lc3Exprsn, binNumber):
-#     lc3Exprsn = lc3Exprsn + " "
-#     fltr1Access = ["ADD","AND","LD","LDI","LDR","LEA","NOT","ST","STI","STR"]
-#     fltr2Access = ["JMP","JSR","JSRR","BR"]
-#     # These values we automatically know
-#     if "TRAP" or "RTI" or "RET" in lc3Exprsn:
-#         return lc3Exprsn
-#     if lc3Exprsn in fltr1Access:
-#         # give only the 3 digits for the DR or SR
-#         try:
-#             lc3Exprsn += fltr1Prcs1(binNumber)
-#         except Exception:
-#             print("There is something wrong with the register of your SR or " +
-#                   "DR.")
-#
-#     elif lc3Exprsn in fltr2Access:
-#         try:
-#             # must be a code with JMP, JSR, JSRR, or BR
-#             fltr2Prcs1(lc3Exprsn, binNumber[4:])
-#     else:
-#         return False
-#
-# def oppCodes(binNumber):
-#     trapVectors = {
-#                 "0010 0000":"x20","0010 0001":"x21",
-#                 "0010 0010":"x21","0010 0011":"x23",
-#                 "0010 0101":"x25"
-#                 }
-#     specOppCodes = {
-#                  "01001":"JSR", "01000": "JSRR"
-#                  }
-#     lc3OppCodes = {
-#                 "0001":"ADD","0101":"AND","0000":"BR",
-#                 "1100":"JMP","0100":"JSR","0010":"LD",
-#                 "1010":"LDI","0110":"LDR","1110":"LEA",
-#                 "1001":"NOT","1100":"RET","1000":"RTI",
-#                 "0011":"ST","1011":"STI","0111":"STR"
-#                 }
-#     #JSR and JSRR checker
-#     if binNumber[:3] == "0100":
-#         #specOppCodes dictionary is used JSR and JSRR
-#         lc3Code = specOppCodes.get(binNumber[:4],"-1")
-#         if lc3Code is "-1":
-#             return False
-#         else:
-#             return lc3Code
-#     #TRAP Code Checker
-#     elif binNumber[:3] == "1111":
-#         #trapVectors dictionary is used
-#         vector = trapVectors.get(binNumber[:3],"-1")
-#         if vector is "-1":
-#             return False
-#         else:
-#             return "TRAP " + vector
-#     else:
-#         #lc3OppCodes dictionary is used
-#         lc3Code = lc3OppCodes.get(binNumber[:3],"-1")
-#         if lc3Code is "-1":
-#             return False
-#         elif lc3Code is "RET" or lc3Code is "RTI":
-#             # checking if it is fully the value for RET/RTI
-#             if binNumber is "1100000111000000" or "1000000000000000":
-#                 return lc3Code
-#             else:
-#                 return False
-#         else:
-#             return lc3Code
+
+def simplifyTraps(expression):
+    simplify = {
+             "TRAP x20":"GETC", "TRAP x21":"OUT", "TRAP x22":"PUTS",
+             "TRAP x23":"IN", "TRAP x24":"PUTSP", "TRAP x25":"HALT",
+             }
+    result = simplify.get(expression,"-1")
+    return result
+
 def trapVectors(vector):
     vectors = {
             "00100000": "x20",
@@ -122,24 +51,36 @@ def getRegister(registerCand):
 
 def immdConverter(number):
     if number[0] is '1':
-        # need to do two's complement
-        result = ""
-        indexPos = 0
+        # need to do two's complement!
+        # zero needs to be there because
+        # we take away the signed bit;
+        # will mess up value if missing signed bit
+        result = "0"
+        # get rid of the signed bit
+        numTemp = number[1:]
+        # looping over string backwards
+        indexPos = len(numTemp) - 1
         indexOfLast1 = 0
-        #get rid of the signed bit and reverses string
-        for i in number[1:]:
+        while indexPos >= 0:
+            i = numTemp[indexPos]
             if i is '1':
                 indexOfLast1 = indexPos
-            indexPos += 1
+                break
+            else:
+                indexPos -= 1
+        # reset indexPos counter for next loop which rebuilds string
         indexPos = 0
-        for i in number[1:]:
-            if indexPos is not indexOfLast1:
+        for i in numTemp:
+            if indexPos < indexOfLast1:
+                # flipping the bits
                 if i is "1":
                     result += "0"
                 else:
                     result += "1"
             else:
-                result += "1"
+                # keep the 1 there (adding 1)
+                result += i
+
             indexPos += 1
         finalResult = "#-" + bin2Dec(result)
         return finalResult
@@ -148,57 +89,80 @@ def immdConverter(number):
         return finalResult
 
 def pcOffset(offset, pc):
-    number = int(pc) + int(immdConverter(offset)[1:])
+    # converts to decimal then converts to hex
+    number = hex(int(immdConverter(offset)[1:]) +
+           int("0x" + pc,16)).upper()[2:]
     return "x" + str(number)
 
 def checkBinary(binaryNumber):
     criteria = {
-		     "^[0].{2}[1][0-1].{5}[0].{2}[0-1].{2}$":'add1',
-             "^[0].{2}[1][0-1].{5}[1][0-1].{4}$": "add2",
-             "^[0][1][0][1][0-1].{5}[0].{2}[0-1].{2}$":'and1',
-             "^[0][1][0][1][0-1].{5}[1][0-1].{4}$": "and2",
-             "^[0][0][0][0][0-1].{11}$": "br",
-             "^[1][1][0][0][0].{2}[0-1].{2}[0].{5}$":"jmp",
-             "^[0][1][0][0][1][0-1].{10}$": "jsr",
-             "^[0][1][0][0][0][0].{2}[0-1].{2}[0].{5}$":"jsrr",
-             "^[0][0][1][0][0-1].{11}$":"ld",
-             "^[1][0][1][0][0-1].{11}$":"ldi",
-             "^[0][1][1][0][0-1].{11}$":"ldr",
-             "^[1][1][1][0][0-1].{11}$":"lea",
-             "^[1][0][0][1][0-1].{5}[1].{5}$":"not",
-             "^[1][1][0].{3}[1].{2}[0].{5}]$":"ret",
-             "^[1][0].{14}$":"rti",
-             "^[0][0][1][1][0-1].{11}$":"st",
-             "^[1][0][1][1][0-1].{11}$":"sti",
-             "^[0][1][1][1][0-1].{11}$":"str",
-             "^[1].{3}[0].{2}[0-1].{7}$":"trap"
+		     "^([0][0][0][1])([0-1].{5})([0].{2})([0-1].{2})$":'add1',
+             "^([0][0][0][1])([0-1].{5})([1])([0-1].{4})$": "add2",
+             "^([0][1][0][1])[0-1].{5}[0].{2}[0-1].{2}$":'and1',
+             "^([0][1][0][1])[0-1].{5}[1][0-1].{4}$": "and2",
+             "^([0][0][0][0])[0-1].{11}$": "br",
+             "^([1][1][0][0])([0].{2})([0-1].{2})([0].{5})$":"jmp",
+             "^([0][1][0][0])[1][0-1].{10}$": "jsr",
+             "^([0][1][0][0])([0][0][0])([0-1].{2})([0].{5})$":"jsrr",
+             "^([0][0][1][0])([0-1].{11})$":"ld",
+             "^([1][0][1][0])([0-1].{11})$":"ldi",
+             "^([0][1][1][0])([0-1].{11})$":"ldr",
+             "^([1][1][1][0])([0-1].{11})$":"lea",
+             "^([1][0][0][1])([0-1].{5})([1].{5})$":"not",
+             "^([1][1][0][0])([0].{2})([1][1][1])([0].{5})$":"ret",
+             "^([1][0][0][0])([0].{11})$":"rti",
+             "^([0][0][1][1])([0-1].{11})$":"st",
+             "^([1][0][1][1])([0-1].{11})$":"sti",
+             "^([0][1][1][1])([0-1].{11})$":"str",
+             "^([1][1][1][1])([0][0][0][0])([0-1].{7})$":"trap"
              }
     keys = criteria.keys()
-    for expression in keys:
+    answer = ""
+    for expression in criteria:
         statement = re.compile(expression)
-        result = bool(re.search(statement, binaryNumber))
+        result = bool(re.search(statement, binaryNumber.strip(" ")))
         if result:
-            return criteria.get(expression,"-1")
+            answer = criteria.get(expression,"-1")
+    return answer
 
 def hex2Bin(hexString):
-    binNumber = "{0:16b}".format(int(hexString,16))
-    # strip any unecessary space from format add 0
-    # to front and cut at 16th bit
-    return "0" + binNumber.strip(" ")[:17]
-
+    temp = hexString.upper()
+    hexDecode = {
+              "A":"10",
+              "B":"11",
+              "C":"12",
+              "D":"13",
+              "E":"14",
+              "F":"15"
+              }
+    binList =  []
+    for character in temp:
+        if character in hexDecode:
+            binList.append(hexDecode.get(character, ""))
+        else:
+            binList.append(character)
+    binary = ""
+    for i in binList:
+        binaryVersion = bin(int(i))[2:]
+        if len(binaryVersion) < 4:
+            numOfZeros = 4 - len(binaryVersion)
+            binary += numOfZeros * "0" + binaryVersion
+        else:
+            binary += binaryVersion
+    return binary
 
 def bin2LC3(hexString, pc):
     lc3Exprsn = ""
     binNum = hex2Bin(hexString).strip(" ")
     getExpression = {
       "add1":"ADD " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + getRegister(binNum[13:16]),
+      getRegister(binNum[7:10]) + ", " + getRegister(binNum[13:16]),
       "add2":"ADD " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + immdConverter(binNum[11:16]),
+      getRegister(binNum[7:10]) + ", " + immdConverter(binNum[11:16]),
       "and1":"AND " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + getRegister(binNum[13:16]),
+      getRegister(binNum[7:10]) + ", " + getRegister(binNum[13:16]),
       "and2":"AND " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + immdConverter(binNum[11:16]),
+      getRegister(binNum[7:10]) + ", " + immdConverter(binNum[11:16]),
       "br":"BR" + getNZP(binNum[4:7]) + " " + pcOffset(binNum[7:16],pc),
       "jmp":"JMP " + getRegister(binNum[7:10]),
       "jsr":"JSR " + pcOffset(binNum[5:16],pc),
@@ -207,7 +171,7 @@ def bin2LC3(hexString, pc):
       "ldi":"LDI " + getRegister(binNum[4:7]) + ", " +
       pcOffset(binNum[7:16],pc),
       "ldr":"LDR " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + immdConverter(binNum[10:16]),
+      getRegister(binNum[7:10]) + ", " + immdConverter(binNum[10:16]),
       "lea":"LEA " + getRegister(binNum[4:7]) + ", " +
       pcOffset(binNum[7:16],pc),
       "not":"NOT " + getRegister(binNum[4:7]) + ", " +
@@ -218,8 +182,8 @@ def bin2LC3(hexString, pc):
       "sti":"STI " + getRegister(binNum[4:7]) + ", " +
       pcOffset(binNum[7:16],pc),
       "str":"STR " + getRegister(binNum[4:7]) + ", " +
-      getRegister(binNum[7:10]) + "," + immdConverter(binNum[10:16]),
-      "trap":"TRAP" + trapVectors(binNum[8:16])
+      getRegister(binNum[7:10]) + ", " + immdConverter(binNum[10:16]),
+      "trap":simplifyTraps("TRAP " + trapVectors(binNum[8:16]))
       }
     binCheck = checkBinary(binNum)
     if binCheck:
@@ -229,23 +193,6 @@ def bin2LC3(hexString, pc):
         else:
             return lc3Expression
     else:
-        return "Incorrect Input."
+        return "Something is wrong with the format of the input."
 
-    # # Getting oppCode
-    # try:
-    #     lc3Exprsn += oppCodes(binNumber) + '\s'
-    # #TODO make sure that this error goes through the gui
-    # except Exception:
-    #     print("The hexidecimal number provided is an unacceptable value",
-    #           "for the LC3.")
-    # # Allows for the binary to be filtered through
-    # # strip on lc3Exprsn for precaution.
-    # result = filters(lc3Exprsn.strip(" "), binNumber)
-    # return result
-
-print(bin2LC3("5020","3000"))
-
-
-
-
-#TODO might want to get rid of the get function for dictionaries because will return -1 if false
+print(bin2LC3("C100","3007"))
